6178c7b3ac7c6971a270f50a08f8224d310d8395
DarYama275 <h3.sep.18@gmail.com>
2026-09-25 00:41:41 +0900

    Use safe literal escaping for inline assets

--- scripts/make-single-html.mjs
+++ scripts/make-single-html.mjs
@@ -10,7 +10,7 @@ html = await replaceAllAsync(
   /<link\s+rel=["']stylesheet["']\s+crossorigin\s+href=["']([^"']+)["']\s*\/?\s*>/g,
   async (_match, href) => {
     const css = await readFile(resolve(distDir, href.replace(/^\//, '')), 'utf8');
-    return `<style>\n${css.replace(/<\\/style/gi, '<\\\\/style')}\n</style>`;
+    return `<style>\n${css.replaceAll('</style', '<\\/style')}\n</style>`;
   },
 );
 
@@ -19,7 +19,7 @@ html = await replaceAllAsync(
   /<script\s+type=["']module["']\s+crossorigin\s+src=["']([^"']+)["']><\/script>/g,
   async (_match, src) => {
     const js = await readFile(resolve(distDir, src.replace(/^\//, '')), 'utf8');
-    return `<script>\n${js.replace(/<\\/script/gi, '<\\\\/script')}\n</script>`;
+    return `<script>\n${js.replaceAll('</script', '<\\/script')}\n</script>`;
   },
 );
 
